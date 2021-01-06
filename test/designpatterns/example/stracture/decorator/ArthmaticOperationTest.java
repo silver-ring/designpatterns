4ac113610f44edd4e0272f354f11042526e504c8
Mohamed ElBahrawy <eng.mohamed.morsy.mohamed@gmail.com>
2021-01-07 00:04:46 +0200

    add command and chain of responsibility patterns

--- test/designpatterns/example/stracture/decorator/ArthmaticOperationTest.java
+++ test/designpatterns/example/stracture/decorator/ArthmaticOperationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AlgorithmicOperationTest {
+public class ArthmaticOperationTest {
 
     @Test
     public void makeAlgorithmicOperationsWithExtra() {
